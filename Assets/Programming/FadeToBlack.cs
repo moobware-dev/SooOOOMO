138d50d828f349610640cbbfb76ac9287b91ebd1
Moobware Dev <dev@moobware.com>
2018-07-04 22:44:47 -0500

    "teaster scene" utilizing the models and animations we've made over the last few streams

---
+++ Assets/Programming/FadeToBlack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeToBlack : MonoBehaviour
+{
+    public Image screenFaderImage;
+    public Color fadeColor;
+    public Text moreToComeText;
+    public float secondsUntilFadeStarts;
+    public float fadeDuration;
+    private float timeWhenFadeShouldBeFull;
+    private float timeFadeStarted;
+
+    public void Start()
+    {
+        StartCoroutine(WaitThenFadeToBlack(secondsUntilFadeStarts));
+    }
+
+    IEnumerator WaitThenFadeToBlack(float secondsToWait) {
+        yield return new WaitForSeconds(secondsToWait);
+
+        StartCoroutine(CofadeToColorInFrontFromClearInBack());
+    }
+
+    IEnumerator CofadeToColorInFrontFromClearInBack()
+    {
+        timeFadeStarted = Time.time;
+        timeWhenFadeShouldBeFull = timeFadeStarted + fadeDuration;
+
+        while (!screenFaderImage.color.Equals(fadeColor))
+        {
+            float fractionFaded = (Time.time - timeFadeStarted) / (timeWhenFadeShouldBeFull - timeFadeStarted);
+            screenFaderImage.color = Color.Lerp(Color.clear, fadeColor, fractionFaded);
+            yield return null;
+        }
+
+        moreToComeText.gameObject.SetActive(true);
+    }
+
+}
