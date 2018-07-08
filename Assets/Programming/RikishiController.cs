b31f44f8c8ca2516787c8feccdb6e731f3edc9ef
Moobware Dev <dev@moobware.com>
2018-07-08 01:25:35 -0500

    Put together some legit garbage for screenshotsaturday LOL  - Learned about adding animation events to imported animations though, so dats cool

--- Assets/Programming/RikishiController.cs
+++ Assets/Programming/RikishiController.cs
@@ -5,22 +5,57 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class RikishiController : MonoBehaviour
 {
+    public bool isThePlayer;
+    public float shoveForce = 100f;
+    Animator animator;
+    RikishiController enemy;
+    Rigidbody enemyRigidBody;
 
-    Animator m_Animator;
-
+    bool isShoved = false;
+    Vector3 shovedForce;
 
     // Use this for initialization
     void Start()
     {
-        m_Animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        var rikishis = FindObjectsOfType<RikishiController>();
+        enemy = rikishis[0] == this ? rikishis[1] : rikishis[0];
+        enemyRigidBody = enemy.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isThePlayer) {
+            return; 
+        }
+
+        if (Input.anyKeyDown) {
+            animator.SetTrigger("Stop Dancing");
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-            m_Animator.SetTrigger("Hadouken");
+            animator.SetTrigger("Hadouken");
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isShoved) {
+            this.animator.SetTrigger("Shoved");
+            GetComponentInParent<Rigidbody>().AddForce(shovedForce);
+            isShoved = false;
+        }
+    }
+
+    void ShoveForce() {
+        Debug.Log("Animation event received");
+        enemy.GetShoved((enemy.gameObject.transform.position - transform.position) * shoveForce);
+    }
+
+    void GetShoved(Vector3 force) {
+        isShoved = true;
+        shovedForce = force;
     }
 }
