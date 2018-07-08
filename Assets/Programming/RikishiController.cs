602ff8b3f694a3254be924eed609821062ef94be
Moobware Dev <dev@moobware.com>
2018-07-07 22:30:36 -0500

    Just putting stuff together quick to have something remotely playable for my frands

---
+++ Assets/Programming/RikishiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RikishiController : MonoBehaviour
+{
+
+    Animator m_Animator;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_Animator.SetTrigger("Hadouken");
+        }
+    }
+}
