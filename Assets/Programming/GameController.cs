5c9d489a323a01ae776bd4136ff93f326a39c853
Moobware Dev <dev@moobware.com>
2018-07-10 19:28:47 -0500

    Got the behavior right for the "Early Access" banner

--- Assets/Programming/GameController.cs
+++ Assets/Programming/GameController.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public GameObject annoyingBannerPrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+        var canvas = GameObject.FindWithTag("UI");
+        Instantiate(annoyingBannerPrefab, canvas.transform);
 	}
 	
 	// Update is called once per frame
