d303477b62464f842b954f81041f31f7e87ec53e
Moobware Dev <dev@moobware.com>
2018-10-31 20:46:19 -0500

    Put everything back in place, shout out to free music peeps

--- Assets/Programming/RikishiPlayerInputProvider.cs
+++ Assets/Programming/RikishiPlayerInputProvider.cs
@@ -6,9 +6,6 @@ using UnityStandardAssets.CrossPlatformInput;
 [RequireComponent(typeof(RikishiController))]
 public class RikishiPlayerInputProvider : MonoBehaviour
 {
-    public float secondsTilInflation = 1f;
-    public float rateOfInflation = 0.1f;
-
     public float doubleTapDelayTime = 0.25f;
     //private KeyCode lastKeyPressed;
     //private float lastKeyPressTime;
@@ -30,48 +27,45 @@ public class RikishiPlayerInputProvider : MonoBehaviour
 
     void Start()
     {
-        //mainCameraTransform = Camera.main.transform;
+        mainCameraTransform = Camera.main.transform;
         playerTransform = GameObject.FindWithTag("Player").transform;
         rikishiController = GetComponent<RikishiController>();
         zeFlabben = GetComponent<ParameterizedFlabbiness>();
         scoreboard = FindObjectOfType<Scoreboard>();
-        //zeFlabben.Flabbiness = scoreboard.GetPlayerScore() * 34f;
-        //rikishiController.shoveForce = zeFlabben.Flabbiness * 3;
-
-        StartCoroutine(DoTheFlab());
-
+        zeFlabben.Flabbiness = scoreboard.GetPlayerScore() * 34f;
+        rikishiController.shoveForce = zeFlabben.Flabbiness * 3;
     }
 
-    //void FixedUpdate()
-    //{
-    //    //var fromCameraToPlayer = mainCameraTransform.transform.position - playerTransform.position; // this drew a line from the camera to the player
-    //    var fromCameraToPlayerOpposite = playerTransform.position - mainCameraTransform.transform.position; // this drew a line from the player in the opposite direction of the camera :D which is what I want!
-    //    Debug.DrawRay(playerTransform.position, fromCameraToPlayerOpposite, Color.blue);
+    void FixedUpdate()
+    {
+        //var fromCameraToPlayer = mainCameraTransform.transform.position - playerTransform.position; // this drew a line from the camera to the player
+        var fromCameraToPlayerOpposite = playerTransform.position - mainCameraTransform.transform.position; // this drew a line from the player in the opposite direction of the camera :D which is what I want!
+        Debug.DrawRay(playerTransform.position, fromCameraToPlayerOpposite, Color.blue);
 
-    //    var playerDesiredRotation = Vector3.ProjectOnPlane(fromCameraToPlayerOpposite, Vector3.up); // line parallel to ground instead of at an angle
-    //    var angleOfDesiredRotation = Vector3.SignedAngle(playerDesiredRotation, playerTransform.forward, Vector3.up) * -1;
-    //    var debugPlayerAimRay = new Vector3(playerTransform.forward.x, playerTransform.forward.y, playerTransform.forward.z) * DebugPlayerAimRayLength;
-    //    debugPlayerAimRay = Quaternion.AngleAxis(angleOfDesiredRotation, Vector3.up) * debugPlayerAimRay;
-    //    Debug.DrawRay(playerTransform.position, debugPlayerAimRay, Color.green);
+        var playerDesiredRotation = Vector3.ProjectOnPlane(fromCameraToPlayerOpposite, Vector3.up); // line parallel to ground instead of at an angle
+        var angleOfDesiredRotation = Vector3.SignedAngle(playerDesiredRotation, playerTransform.forward, Vector3.up) * -1;
+        var debugPlayerAimRay = new Vector3(playerTransform.forward.x, playerTransform.forward.y, playerTransform.forward.z) * DebugPlayerAimRayLength;
+        debugPlayerAimRay = Quaternion.AngleAxis(angleOfDesiredRotation, Vector3.up) * debugPlayerAimRay;
+        Debug.DrawRay(playerTransform.position, debugPlayerAimRay, Color.green);
 
-    //    rikishiController.SetDesiredAimTarget(debugPlayerAimRay + playerTransform.position);
+        rikishiController.SetDesiredAimTarget(debugPlayerAimRay + playerTransform.position);
 
-    //    float h = CrossPlatformInputManager.GetAxis("Horizontal");
-    //    float v = CrossPlatformInputManager.GetAxis("Vertical");
-    //    var movementVector = v * playerTransform.forward + h * playerTransform.right;
-    //    rikishiController.Move(movementVector);
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+        var movementVector = v * playerTransform.forward + h * playerTransform.right;
+        rikishiController.Move(movementVector);
 
-    //    if (shouldDodgeRight)
-    //    {
-    //        shouldDodgeRight = false;
-    //        rikishiController.DodgeRight();
-    //    }
+        if (shouldDodgeRight)
+        {
+            shouldDodgeRight = false;
+            rikishiController.DodgeRight();
+        }
 
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        rikishiController.AttemptShove();
-    //    }
-    //}
+        if (Input.GetMouseButtonDown(0))
+        {
+            rikishiController.AttemptShove();
+        }
+    }
 
     private void Update()
     {
@@ -88,14 +82,5 @@ public class RikishiPlayerInputProvider : MonoBehaviour
     private bool PlayerDoubleTapped(KeyCode key)
     {
         return (Time.time - LastTimeKeyTapped[key]) < doubleTapDelayTime;
-    }
-
-    IEnumerator DoTheFlab()
-    {
-        yield return new WaitForSeconds(secondsTilInflation);
-        while (zeFlabben.Flabbiness < 100) {
-            zeFlabben.Flabbiness += rateOfInflation * Time.deltaTime;
-            yield return null;
-        }
     }
 }
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ A Sumo Wrastlin' game.  Here's the status of the project: [trello][1], and the l
 Contact the maintainers on [twitter][3] if you want.
 
 Credit to the following folks for their free stuff that we ganked:
+ - (Music) Ghost Surf Rock by [Loyalty Freak Music][6] is in the [Public Domain][7]
  - (Font) ChunkFive by [theleagueofmoveabletype][4] is licensed under a [SIL Open Font License][5].
 
  [1]: https://trello.com/b/JXJxHQfP/the-joy-of-game-dev-stream
@@ -14,3 +15,5 @@ Credit to the following folks for their free stuff that we ganked:
  [3]: https://twitter.com/moobware
  [4]: https://github.com/theleagueof/chunk
  [5]: http://scripts.sil.org/OFL
+ [6]: http://freemusicarchive.org/music/Loyalty_Freak_Music/WITCHY_BATTY_SPOOKY_HALLOWEEN_IN_SEPTEMBER_/Ghost_Surf_Rock
+ [7]: https://creativecommons.org/publicdomain/zero/1.0/
