962f9c8a54f18159644ef4b5764b02a2de54193d
Moobware Dev <dev@moobware.com>
2019-07-30 22:09:07 -0500

    Welp, let's just simplify things because my mathy way of using the angle didn't work AGAIN

--- Assets/Programming/Experiments/TrackTarget.cs
+++ Assets/Programming/Experiments/TrackTarget.cs
@@ -9,34 +9,30 @@ public class TrackTarget : MonoBehaviour
     public Transform Target;
     public float TrackSpeed;
 
-    private Vector3 _trackedPosition = Vector3.zero;
+    private Transform _trackingTransform;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _trackingTransform = new GameObject().transform;
+        _trackingTransform.position = transform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var distanceFromMeToYou = Target.position - transform.position;
-        var angleFromMyFrontToYou = Vector3.SignedAngle(transform.forward, distanceFromMeToYou, Vector3.up);
-        Debug.Log("muh angle: " + angleFromMyFrontToYou);
-
-        var angleStep = angleFromMyFrontToYou * Time.deltaTime * TrackSpeed;
-        Debug.Log("angle step: " + angleStep);
-
-        var proportionateRotation = Quaternion.AngleAxis(angleStep, Vector3.up);
-        _trackedPosition = transform.forward + proportionateRotation * transform.forward;
-
-        transform.LookAt(_trackedPosition);
+        _trackingTransform.position = Vector3.Lerp(_trackingTransform.position, Target.position, Time.deltaTime * TrackSpeed);
+        transform.LookAt(_trackingTransform.position);
     }
 
     private void OnDrawGizmos()
     {
+        if (_trackingTransform == null)
+        {
+            return;
+        }
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(_trackedPosition, TrackingGizmoRadius);
+        Gizmos.DrawWireSphere(_trackingTransform.position, TrackingGizmoRadius);
     }
 }
